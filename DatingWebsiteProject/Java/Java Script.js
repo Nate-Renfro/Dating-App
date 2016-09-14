2192dee0bc475043cdf4523cf07f81ea3fe4cc92
Nathan Renfro <nathan.renfro490@524-11a5g8wn.local>
2016-09-13 17:03:14 -0700

    Everything is finished

---
+++ DatingWebsiteProject/Java/Java Script.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#preview')
+                    .attr('src', e.target.result)
+                    .width(150)
+                    .height(200);
+            };
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+</script>
